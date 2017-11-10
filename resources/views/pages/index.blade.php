6a638c9c1e3144e67ca33a943d4ba4eefcfcb00c
Belchenkov <u608110@gmail.com>
2017-11-10 20:24:12 +0300

    Basic Routing & Controllers

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{config('app.name', 'LSAPP')}}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Barlow:400,700" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Barlow', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome To Laravel</h1>
+    <p>This is the laravel application</p>
+</body>
+</html>
